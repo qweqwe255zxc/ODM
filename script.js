8e56b02d369484e48f905f9818eb0defb12611d7
Андрей Бакушин <bakushinandrey@MacBook-Air-Andrej.local>
2025-05-02 14:22:03 +0300

    4

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
 let lastScroll = 0;
-const defaultOffset = 200;
+const defOffset = 200
+const nav_local = document.querySelector('.nav-local');
 const header = document.querySelector('.header');
 
 const scrollPosition = () => window.pageYOffset || document.documentElement.scrollTop;
-const containHide = () => header.classList.contains('hide');
+const containHide = () => nav_local.classList.contains('hide');
 
 window.addEventListener('scroll', () => {
-    if(scrollPosition() > lastScroll && !containHide() && scrollPosition() > defaultOffset) {
-        header.classList.add('hide');
-        console.log('down');
-        
+
+    if (scrollPosition() > lastScroll && !containHide() && scrollPosition() > defOffset) {
+        nav_local.classList.add('hide');
+        header.classList.add('shrink-header');
     }
-    else if(scrollPosition() < lastScroll && containHide()){
-        header.classList.remove('hide');
-        console.log('up');
-    
+    else if (scrollPosition() < lastScroll && containHide()) {
+        nav_local.classList.remove('hide');
+        header.classList.remove('shrink-header');
     }
 
     lastScroll = scrollPosition();
-})
+});
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', e => {
+        e.preventDefault();
+        document.querySelector(anchor.getAttribute('href')).scrollIntoView({ behavior: 'smooth' });
+    });
+});
